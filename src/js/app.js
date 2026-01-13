c4cf9b5a6286f10eabffb4a7ee71e3acef361931
Margo <romanova184@yandex.ru>
2026-01-13 14:22:52 +0300

    Next commit

--- src/js/app.js
+++ src/js/app.js
@@ -2,15 +2,6 @@
 import ManageList from "./manageList";
 import SaveInStorage from "./saveInStorage";
 
-// comment this to pass build
-const unusedVariable = "variable";
-
-// for demonstration purpose only
-export default function demo(value) {
-  return `Demo: ${value}`;
-}
-
-console.log("app.js included");
 let save = new SaveInStorage(localStorage);
 let list = new ManageList(save);
 list.loadList()
